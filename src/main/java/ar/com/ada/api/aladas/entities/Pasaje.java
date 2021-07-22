246d5ffe6ed049558e4f48edf554cd1e6ddef667
MelinaMontes <you@example.com>
2021-07-22 19:57:12 -0300

    agrego los atributos de las entidades junto con getters, setters y anotaciones para vincular con la db

--- src/main/java/ar/com/ada/api/aladas/entities/Pasaje.java
+++ src/main/java/ar/com/ada/api/aladas/entities/Pasaje.java
@@ -1,5 +1,26 @@
 package ar.com.ada.api.aladas.entities;
 
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "pasaje")
 public class Pasaje {
-    
+
+    @Id
+    @Column (name = "pasaje_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pasajeId;
+
+    @OneToOne
+    @JoinColumn (name = "reserva_id", referencedColumnName = "pasaje_id")
+    private Reserva reserva;
+
+    @Column (name = "fecha_emision")
+    private Date fechaEmision;
+
+    @Column (name = "info_pago")
+    private String infoPago;
+
+
 }
--- src/main/java/ar/com/ada/api/aladas/entities/Pasajero.java
+++ src/main/java/ar/com/ada/api/aladas/entities/Pasajero.java
@@ -1,5 +1,32 @@
 package ar.com.ada.api.aladas.entities;
 
-public class Pasajero {
-    
+import java.util.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pasajero")
+public class Pasajero extends Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pasajero_id")
+    private Integer pasajeroId;
+
+    @OneToMany(mappedBy = "pasajero", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reserva> reservas = new ArrayList<>();
+
+    public Integer getPasajeroId() {
+        return pasajeroId;
+    }
+
+    public void setPasajeroId(Integer pasajeroId) {
+        this.pasajeroId = pasajeroId;
+    }
+
+    public void agregarReserva(Reserva reserva) {
+        this.reservas.add(reserva);
+        reserva.setPasajero(this);
+    }
+
 }
+
